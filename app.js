dd05f94b4b8c9f18b160f2212f2576665cf1f983
Debasis Das <debasis.das@in.ibm.com>
2017-04-20 02:11:00 -0500

    removed favicon

--- app.js
+++ app.js
@@ -12,9 +12,6 @@ var port = process.env.VCAP_APP_PORT || 3000;
 
 // all environments
 app.set('port', port || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
